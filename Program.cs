597e2c683289640a6be88a95fcfd08666c478098
prvabeogim <prvabeogim@outlook.com>
2026-03-16 13:36:08 +0100

    v2

--- Program.cs
+++ Program.cs
@@ -11,12 +11,12 @@ namespace poligon_3_9_2026b
         static void Main(string[] args)
         {
             //Jovan Katancevic
-            Tacka a = new Tacka(3, 2);
-            Tacka b = new Tacka(-2, 3);
-            Tacka o = new Tacka();
-            Vektor oa = new Vektor(o, a);
-            Vektor ob = new Vektor(o, b);
-            Console.WriteLine(Vektor.VP(oa, ob));
+            Tacka a = new Tacka(4, 5);
+            Tacka b = new Tacka(5, 5);
+            Tacka prva = new Tacka(3,2);
+            Tacka druga = new Tacka(7, 6);
+            Vektor A = new Vektor(prva, druga);
+            Console.WriteLine(Ravan.sis(A,a,b));
             Console.ReadLine();
         }
     }
