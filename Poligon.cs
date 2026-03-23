f997d80de02bb5f361858465eab2b43d68a161cf
prvabeogim <prvabeogim@outlook.com>
2026-03-23 18:45:33 +0100

    v3

---
+++ Poligon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon_3_9_2026b
+{
+    internal class Poligon
+    {
+        public int broj_temena;
+        public Tacka[] teme;
+        public Poligon(int n) 
+        {
+            broj_temena = n;
+            teme = new Tacka[n];
+        }
+        public static Poligon unos()
+        {
+            return null;
+        }
+        public void stampa()
+        {
+            Console.WriteLine();
+        }
+        public void snimi()
+        {
+
+        }
+        public static Poligon ucitaj()
+        {
+            return null;
+        }
+    }
+}
